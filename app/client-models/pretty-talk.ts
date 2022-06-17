d5af08125f3114e2f19e6ebdc97b0a1aa3bad18b
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2022-06-17 11:00:08 +0200

    upcoming and past talks

--- app/client-models/pretty-talk.ts
+++ app/client-models/pretty-talk.ts
@@ -1,3 +1,7 @@
 import { Talk } from "@prisma/client";
 
-export type PrettyTalk = Omit<Talk, "date"> & { prettyDate: string };
+export type PrettyTalk = Omit<Talk, "date"> & {
+  date: Date;
+  prettyDate: string;
+  isUpcoming: boolean;
+};
--- app/components/talk-list.tsx
+++ app/components/talk-list.tsx
@@ -3,7 +3,7 @@ import { Link } from "@remix-run/react";
 import { PrettyTalk } from "~/client-models/pretty-talk";
 
 export interface TalkListProps {
-  talks: PrettyTalk[];
+  talks: { upcoming: PrettyTalk[]; past: PrettyTalk[] };
 }
 
 export function TalkList({ talks }: TalkListProps) {
@@ -13,8 +13,16 @@ export function TalkList({ talks }: TalkListProps) {
       <Link className="p-button" to="./add">
         Add new Talk
       </Link>
+      <h3 className="mt-6 mb-3 text-2xl font-bold">Upcoming</h3>
       <div className="grid grid-cols-3 gap-5">
-        {talks.map((talk) => (
+        {talks.upcoming.map((talk) => (
+          <TalkCard key={talk.id} talk={talk}></TalkCard>
+        ))}
+      </div>
+
+      <h3 className="mt-6 mb-3 text-2xl font-bold">Past</h3>
+      <div className="grid grid-cols-3 gap-5">
+        {talks.past.map((talk) => (
           <TalkCard key={talk.id} talk={talk}></TalkCard>
         ))}
       </div>
